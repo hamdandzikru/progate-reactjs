2a3fd397bb5fd4e89f546070fe03a881c0d996ff
hamdandzikru <hamdanrobbi@gmail.com>
2021-04-01 14:42:34 +0700

    state

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
 class App extends React.Component {
+  // Nyatakan constructor dibawah
+  constructor(props) {
+    super(props);
+    // Penerapan state
+    this.state = {name: 'Ninja Ken'};
+    
+  }
+  
   render() {
     return (
     	<div>
     	  <h1>Halo, Ninja Ken!</h1>
-        {/* Tambahkan event onClick didalam tag button, yang berfungsi untuk mencetak nama di console */}
         <button onClick={() => {console.log('Guru Domba')}}>Guru Domba</button>
-        
-        {/* Tambahkan event onClick didalam tag button, yang berfungsi untuk mencetak nama di console */}
         <button onClick={() => {console.log('Ninja Ken')}}>Ninja Ken</button>
-        
       </div>
     );
   }
