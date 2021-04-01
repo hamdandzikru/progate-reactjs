386bf29cde6c41d00455d472cf0941a300973d94
hamdandzikru <hamdanrobbi@gmail.com>
2021-04-01 14:43:31 +0700

    state modfy

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 
 class App extends React.Component {
-  // Nyatakan constructor dibawah
   constructor(props) {
     super(props);
-    // Penerapan state
     this.state = {name: 'Ninja Ken'};
-    
   }
   
   render() {
     return (
     	<div>
-    	  <h1>Halo, Ninja Ken!</h1>
-        <button onClick={() => {console.log('Guru Domba')}}>Guru Domba</button>
-        <button onClick={() => {console.log('Ninja Ken')}}>Ninja Ken</button>
+    	  <h1>Halo, {this.state.name}!</h1>
+    	  {/* Ubah state untuk event onClick */}
+        <button onClick={() => {this.setState({name: 'Guru Domba'})}}>Guru Domba</button>
+        
+    	  {/* Ubah state untuk event onClick */}
+        <button onClick={() => {this.setState({name: 'Ninja Ken'})}}>Ninja Ken</button>
+        
       </div>
     );
   }
