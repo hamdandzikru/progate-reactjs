9b212d2bb47b03353c1c080602d81933d0fa97f1
hamdandzikru <hamdanrobbi@gmail.com>
2021-04-01 14:42:00 +0700

    modify

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,13 @@ class App extends React.Component {
     return (
     	<div>
     	  <h1>Halo, Ninja Ken!</h1>
-    	  {/* Tambahkan tag <button> dengan teks "Guru Domba" */}
-    	  <button>Guru Domba</button>
-    	  
-    	  {/* Tambahkan tag <button> dengan teks "Ninja Ken" */}
-    	  <button>Ninja Ken</button>
-    	  
-    	</div>
+        {/* Tambahkan event onClick didalam tag button, yang berfungsi untuk mencetak nama di console */}
+        <button onClick={() => {console.log('Guru Domba')}}>Guru Domba</button>
+        
+        {/* Tambahkan event onClick didalam tag button, yang berfungsi untuk mencetak nama di console */}
+        <button onClick={() => {console.log('Ninja Ken')}}>Ninja Ken</button>
+        
+      </div>
     );
   }
 }
